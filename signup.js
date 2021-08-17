e748d535c021b93ff96d0ccc6632266328dc65af
AbdulAhad <49091109+Aadichohan@users.noreply.github.com>
2021-08-17 14:50:10 +0500

    removed commented code

--- signup.js
+++ signup.js
@@ -16,19 +16,17 @@ const SignUpSetup = () => {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var rollId = document.getElementById('role').value;
-    //const signUpConst = new signUpClass(id, uname, email, password, rollId);
-    //SignUpStorage.push(signUpConst);
+
    let login = localStorage.getItem('login');
-   //console.log('login ', login);
+
       login = (login) ? JSON.parse(login) : [];
-      //  console.log('setLogin ', login);
+
        login.map((SignUp,i)=>{
            id = SignUp.id + 1 ;
            SignUpStorage.push(SignUp);
         });
         
 
-   // console.log('map ', id);
     const signUpConst = new signUpClass(id, uname, email, password, rollId);
      SignUpStorage.push(signUpConst);
      localStorage.setItem('login',JSON.stringify(SignUpStorage)); 
