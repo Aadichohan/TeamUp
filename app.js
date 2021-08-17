aa3aea7f192321ccea5c26f7c12573ced0fcb188
AbdulAhad <49091109+Aadichohan@users.noreply.github.com>
2021-08-17 19:02:07 +0500

    2nd commit

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-class role{
-  constructor(id,name){
-      this.id    = id,
-      this.name  = name
+class LoginClass{
+  constructor(isLogin){
+      this.isLogin  = isLogin 
   }
 }
 
+const LoginSetup = () => {
+    let SignUpStorage = [];
+    let id = 1;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+   let login = localStorage.getItem('login');
+
+      login = (login) ? JSON.parse(login) : [];
+
+      objIndex = login.findIndex((obj => obj.email == email && obj.password == password && obj.isEnable == 1));
+     // console.log(objIndex,' objIndex');
+    //  console.log("Before update: ", login[objIndex])
+
+//Update object's name property.
+login[objIndex].isLogin = true;
+
+//Log object to console again.
+console.log("After update: ", login[objIndex])
 
 
-const roleSetup = () => {
-  let getLogin =  localStorage.getItem('login');
-   console.log(getLogin);
-    if(getLogin === null){
-       localStorage.setItem('login', []);
-    }
-  let roleList = ['TeamLead','Employee'];
-  let roleStorage = [];
-  roleList.map((roles,i)=>{
-      i = i + 1;
-   const roleConst = new role(i,roles);
-       roleStorage.push(roleConst);
-       console.log(roleStorage);
-       var option = document.createElement("option");
-       option.text = roles;
-       option.value = i;
-       document.getElementById('role').appendChild(option);
-  });
-  localStorage.setItem('roles',JSON.stringify(roleStorage)); 
-  console.log(localStorage.getItem('roles'));
+    // const signUpConst = new LoginClass(true);
+    //  SignUpStorage.push(signUpConst);
+    //  localStorage.setItem('login',JSON.stringify(SignUpStorage)); 
+    // console.log('SignUpStorage ', SignUpStorage);
+
 }
 
 
 {
     //Setup Roles
-    roleSetup();
+   // roleSetup();
     
   
 }
--- signup.js
+++ signup.js
@@ -2,10 +2,18 @@ class signUpClass{
   constructor(id, uname, email, password, roll_id){
       this.id        = id,
       this.uname     = uname,
+      this.email     = email,
       this.password  = password,
-      this.rollId   = roll_id,
-      this.isLogin  = false ,
-      this.isEnable = 1 
+      this.rollId    = roll_id,
+      this.isLogin   = false ,
+      this.isEnable  = 1 
+  }
+}
+
+class role{
+  constructor(id,name){
+      this.id    = id,
+      this.name  = name
   }
 }
 
@@ -32,4 +40,34 @@ const SignUpSetup = () => {
      localStorage.setItem('login',JSON.stringify(SignUpStorage)); 
     // console.log('SignUpStorage ', SignUpStorage);
 
+}
+
+const roleSetup = () => {
+  let getLogin =  localStorage.getItem('login');
+   console.log(getLogin);
+    if(getLogin === null){
+       localStorage.setItem('login', []);
+    }
+  let roleList = ['TeamLead','Employee'];
+  let roleStorage = [];
+  roleList.map((roles,i)=>{
+      i = i + 1;
+   const roleConst = new role(i,roles);
+       roleStorage.push(roleConst);
+       console.log(roleStorage);
+       var option = document.createElement("option");
+       option.text = roles;
+       option.value = i;
+       document.getElementById('role').appendChild(option);
+  });
+  localStorage.setItem('roles',JSON.stringify(roleStorage)); 
+  console.log(localStorage.getItem('roles'));
+}
+
+
+{
+    //Setup Roles
+    roleSetup();
+    
+  
 }
